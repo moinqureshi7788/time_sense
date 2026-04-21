96e0a02ed0ea5c253734f84ce62f371943881571
moinqureshi <moinqureshi7788@gmail.com>
2026-04-21 17:25:20 +0530

    Fix Railway deployment - add 0.0.0.0 binding

--- server/index.js
+++ server/index.js
@@ -25,6 +25,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'TimeSense API is running! 🚀' })
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`)
 })
